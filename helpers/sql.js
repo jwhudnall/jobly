ca8bf549d197befa63ef8d4960475d9db6450814
James Hudnall <jimmyhudnall@gmail.com>
2022-03-13 12:53:04 -0400

    Add documentation explaining sqlForPartialUpdate()

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,22 +1,26 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-// Accepts:
-//  - dataToUpdate: Object of information
-//  - jsToSql: Object of permissable columns eligible to be updated
+/**
+ * Convert object keys into a single SQL variable string. Returns this string, along with
+ * the specified values.
+ * Optionally converts keys to match columns within the database.
+ *
+ * @param {*} dataToUpdate - Object of key:value pairs of information to be updated.
+ * @param {*} jsToSql  - Object of key:value pairs converting JS syntax to match that found in the db.
+ * @returns - Object {
+ *              setCols: SQL-compatible variable string of columns to be updated.
+ *              values: Array of values to be passed in alongside setCols string. }
+ *
+ * ex:  sqlForPartialUpdate( {firstName: 'Aliya', age: 32}, { firstName: "first_name" }) =>
+ *        ['"first_name"=$1', '"age"=$2']
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   // Throws error if empty object is passed
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map(
-    (colName, idx) =>
-      // Maps each key to either the specified change, or the original
-      // if 2nd argument is undefined, defaults to the key name from data
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`
-  );
+  const cols = keys.map((colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`);
 
   return {
     setCols: cols.join(", "),
