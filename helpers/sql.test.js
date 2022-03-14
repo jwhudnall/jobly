905b8273187ae8f8f01fe5869e581345314c74b5
James Hudnall <jimmyhudnall@gmail.com>
2022-03-13 21:24:01 -0400

    Add test suite for sqlForCompanyFilter()

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,6 +1,7 @@
-const { sqlForPartialUpdate } = require("./sql");
+const { sqlForPartialUpdate, sqlForCompanyFilter } = require("./sql");
 
 const data = { name: "ABC", description: "description", numEmployees: 3 };
+const filteredSearch = {};
 
 describe("Update data", () => {
   test("Updates with no key transformations", () => {
@@ -21,5 +22,34 @@ describe("Update data", () => {
     expect(() => {
       sqlForPartialUpdate({}, {});
     }).toThrow("No data"); //https://jestjs.io/docs/expect#tothrowerror
+  });
+});
+
+describe("Filtered Company search", () => {
+  test("Converts all 3 params", () => {
+    const params = { name: "abc", minEmployees: 3, maxEmployees: 10 };
+    result = sqlForCompanyFilter(params);
+    expect(result).toHaveProperty("setCols");
+    expect(result).toHaveProperty("values");
+    expect(result.setCols.match(/AND/g).length).toBe(2);
+    expect(result.values).toEqual(["abc", 3, 10]);
+  });
+  test("Converts search with 2 params", () => {
+    const params = { minEmployees: 3, maxEmployees: 10 };
+    result = sqlForCompanyFilter(params);
+    expect(result).toHaveProperty("setCols");
+    expect(result).toHaveProperty("values");
+    expect(result.setCols.match(/AND/g).length).toBe(1);
+    expect(result.values).toEqual([3, 10]);
+  });
+  test("Converts search with 1 params", () => {
+    const params = { maxEmployees: 10 };
+    result = sqlForCompanyFilter(params);
+    expect(result).toHaveProperty("setCols");
+    expect(result).toHaveProperty("values");
+    expect(result.values).toEqual([10]);
+    expect(() => {
+      result.setCols.match(/AND/g).length;
+    }).toThrowError(TypeError);
   });
 });
