9d5fe1d33f62ecac5d6883af4fffc6f2adf71017
James Hudnall <jimmyhudnall@gmail.com>
2022-03-15 19:50:06 -0400

    Add initial /jobs routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -29,7 +29,7 @@ const router = new express.Router();
  * Authorization required: admin
  */
 // TODO: Add ensureIsAdmin middleware
-router.post("/", async function (req, res, next) {
+router.post("/", ensureIsAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, jobNewSchema);
     if (!validator.valid) {
@@ -49,7 +49,94 @@ router.post("/", async function (req, res, next) {
   }
 });
 
-module.exports = router;
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, company_handle }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - title
+ * - minSalary
+ * - hasEquity
+ *
+ * Authorization required: none
+ */
 
-// tests:
-// - /POST: Invalid company handle
+router.get("/", async function (req, res, next) {
+  const hasParams = Object.keys(req.query).length > 0;
+  let jobs;
+  try {
+    if (hasParams) {
+      const validator = jsonschema.validate(req.query, jobSearchSchema, {
+        preValidateProperty // Pre-Property Validation Hook to cast numerical strings => numbers
+      });
+      if (!validator.valid) {
+        const errs = validator.errors.map((e) => e.stack);
+        throw new BadRequestError(errs);
+      }
+      jobs = await Job.findFiltered(req.query);
+    } else {
+      jobs = await Job.findAll();
+    }
+    return res.json({ jobs });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  =>  { job }
+ *
+ *  Job is { id, title, salary, equity, company_handle }
+ *  Invalid ID returns 404 error natively via Job class.
+ *
+ * Authorization required: none
+ */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const job = await Job.get(req.params.id);
+    return res.json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[id] { fld1, fld2, ... } => { job }
+ *
+ * Patches job data.
+ *
+ * fields can be: { title, salary, equity }
+ *
+ * Returns { handle, title, salary, equity }
+ *
+ * Authorization required: admin
+ */
+
+router.patch("/:id", ensureIsAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, jobUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const job = await Job.update(req.params.id, req.body);
+    return res.json({ job });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization: admin
+ */
+
+router.delete("/:id", async function (req, res, next) {
+  try {
+    await Job.remove(req.params.id);
+    return res.json({ deleted: req.params.id });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
